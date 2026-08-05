95dca5f1ee2b51bd20f8ec7feca1cd9aea135a70
mroczect <mroczect@proton.me>
2026-08-06 01:27:48 +0700

    feat(handler): set default algorithm to Sha256

--- lib_handler/src/constants.rs
+++ lib_handler/src/constants.rs
@@ -2,4 +2,4 @@ use super::enums::Algo;
 
 pub const DEFAULT_TIME_STEP: u64 = 30;
 pub const DEFAULT_DIGITS: u32 = 6;
-pub const DEFAULT_ALGO: Algo = Algo::Sha1;
+pub const DEFAULT_ALGO: Algo = Algo::Sha256;

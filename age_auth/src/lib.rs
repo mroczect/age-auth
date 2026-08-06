b172d9cc793620ac0e684033efe6fd1ed6e80037
mroczect <mroczect@proton.me>
2026-08-06 18:47:03 +0700

    feat(auth): re-export subcrates

--- age_auth/src/lib.rs
+++ age_auth/src/lib.rs
@@ -1,1 +1,4 @@
-
+pub use libage_auth_handler::*;
+pub use libage_authenticator::AgeAuthenticator;
+pub use libage_crypto::{AgeCrypto, encrypt_multiple, generate_keypair};
+pub use libage_otp::AgeOtp;

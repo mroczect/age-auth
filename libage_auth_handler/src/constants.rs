5c4c6b94a08fc69f733624a3ee8262444d662a03
mroczect <mroczect@proton.me>
2026-08-18 12:47:03 +0700

    Update constants.rs

--- libage_auth_handler/src/constants.rs
+++ libage_auth_handler/src/constants.rs
@@ -12,18 +12,6 @@
 //!   TOTP implementations.
 
 use super::enums::Algo;
-
-/// Default time step (in seconds) for TOTP.
-///
-/// Value: `30`
 pub const DEFAULT_TIME_STEP: u64 = 30;
-
-/// Default number of digits for OTP tokens.
-///
-/// Value: `6`
 pub const DEFAULT_DIGITS: u32 = 6;
-
-/// Default hash algorithm for OTP generation.
-///
-/// Value: `Algo::Sha256`
 pub const DEFAULT_ALGO: Algo = Algo::Sha256;
